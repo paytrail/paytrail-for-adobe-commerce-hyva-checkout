03c1bcdbc04d5a50c43877d7d97718e843899b77
Konrad Konieczny <konrad.konieczny@solteq.com>
2025-04-10 09:58:52 +0200

    SQMAGOPC-640: remove debug changes

--- ViewModel/PaytrailPaymentViewModel.php
+++ ViewModel/PaytrailPaymentViewModel.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Paytrail\PaymentServiceHyvaCheckout\ViewModel;
 
-//use Goodahead\ABTesting\Model\ConfigProvider;
-//use Goodahead\ABTesting\Model\CookieProvider;
 use Paytrail\PaymentServiceHyvaCheckout\Model\Config;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
@@ -15,8 +13,6 @@ class PaytrailPaymentViewModel implements ArgumentInterface
      */
     public function __construct(
         protected Config $config,
-//        protected ConfigProvider $abTestingConfig,
-//        protected CookieProvider $cookieProvider
     ) {}
 
     /**
@@ -24,14 +20,6 @@ class PaytrailPaymentViewModel implements ArgumentInterface
      */
     public function canDisplayAdditionalInformation(): bool
     {
-//        if (!$this->abTestingConfig->isCheckoutPaymentMethodsABEnabled()) {
             return $this->config->canShowAdditionalInformation();
-//        }
-
-//        if ($this->cookieProvider->get() == 1) {
-//            return $this->config->canShowAdditionalInformation();
-//        }
-
-        return false;
     }
 }
